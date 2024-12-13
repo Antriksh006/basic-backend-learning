1453393c961789277c615943398b376888cf717c
Antriksh006 <gantriksh9@gmail.com>
2024-12-13 23:13:30 +0530

    utils addition

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ dotenv.config({
 });
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("Error connecting to the database: ", error);
+});
 
 
 
